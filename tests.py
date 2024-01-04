e750ea8c9608c12b908852053ffc25b0712a6c91
Danilo Naiff <danilonaiff1992@gmail.com>
2024-01-04 14:38:30 -0300

    New commit

--- tests.py
+++ tests.py
@@ -77,7 +77,8 @@ class LLMClassifier(lightning.LightningModule):
 def run_test(training_config,
              model_config,
              train_dataloader,
-             valid_dataloader):
+             valid_dataloader,
+             val_check_interval=1):
     decoder_mode = training_config['decoder_mode']
     has_wg = training_config['wg']
     orig_batch_size = training_config['orig_batch_size']
@@ -95,7 +96,7 @@ def run_test(training_config,
                                 accumulate_grad_batches=batch_split,
                                 callbacks=[lr_monitor],
                                 log_every_n_steps=1,
-                                val_check_interval=0.125,
+                                val_check_interval=val_check_interval,
                                 gradient_clip_val=0.5)
     trainer.fit(model=module,
                 train_dataloaders=train_dataloader,
@@ -194,7 +195,8 @@ def test_listops_mini(batch_split=1, num_workers=4, wg=False, decoder_mode="defa
                        'total_epochs': total_epochs,
                        'lr': 0.05,
                        'warmup_steps': 1000}
-    run_test(training_config, model_config, train_dataloader, valid_dataloader)
+    run_test(training_config, model_config, train_dataloader, valid_dataloader,
+             val_check_interval=0.125)
 
 
 def test_listops(batch_split=8, num_workers=23, wg=False, decoder_mode="default"):
@@ -205,7 +207,7 @@ def test_listops(batch_split=8, num_workers=23, wg=False, decoder_mode="default"
     train_dataloader = dataset.train_dataloader(batch_size=batch_size, num_workers=num_workers)
     valid_dataloader = dataset.val_dataloader(batch_size=batch_size, num_workers=num_workers)
     total_epochs = 5000//(len(train_dataloader)//batch_split) + 1
-    config = GPTRConfig(vocab_size=dataset.vocab_size,
+    model_config = GPTRConfig(vocab_size=dataset.vocab_size,
                         context_window=2100,
                         nclasses=10,
                         embedding_dim=512,
@@ -214,12 +216,18 @@ def test_listops(batch_split=8, num_workers=23, wg=False, decoder_mode="default"
                         nhidden=2048,
                         pdrop=0.1
                         )
-    model = GPTRClassifier(config, has_wg=wg, decoder_mode=decoder_mode)
-    module = LLMClassifier(model, warmup_steps=1000*batch_split)
-    trainer = lightning.Trainer(max_epochs=2, accumulate_grad_batches=8)
-    trainer.fit(model=module, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader)
+    training_config = {'decoder_mode': decoder_mode,
+                       'wg': wg,
+                       'orig_batch_size': orig_batch_size,
+                       'batch_split': batch_split,
+                       'total_epochs': total_epochs,
+                       'lr': 0.05,
+                       'warmup_steps': 1000}
+    run_test(training_config, model_config, train_dataloader, valid_dataloader,
+             val_check_interval=0.125)
 
-def test_imdb(batch_split=16, num_workers=23, wg=False, decoder_mode="default"):
+
+def test_imdb(batch_split=32, num_workers=23, wg=False, decoder_mode="default"):
     dataset = IMDB("imdb")
     dataset.setup()
     orig_batch_size = 32
@@ -227,8 +235,8 @@ def test_imdb(batch_split=16, num_workers=23, wg=False, decoder_mode="default"):
     train_dataloader = dataset.train_dataloader(batch_size=batch_size, num_workers=num_workers)
     valid_dataloader = dataset.val_dataloader(batch_size=batch_size, num_workers=num_workers)
     total_epochs = 20000//(len(train_dataloader)//batch_split) + 1
-    config = GPTRConfig(vocab_size=dataset.n_tokens,
-                    context_window=5000,
+    model_config = GPTRConfig(vocab_size=dataset.n_tokens,
+                    context_window=4100,
                     nclasses=2,
                     embedding_dim=512,
                     nheads=8,
@@ -236,10 +244,14 @@ def test_imdb(batch_split=16, num_workers=23, wg=False, decoder_mode="default"):
                     nhidden=2048,
                     pdrop=0.1
                     )
-    model = GPTRClassifier(config, has_wg=wg, decoder_mode=decoder_mode)
-    module = LLMClassifier(model, warmup_steps=1000*batch_split)
-    trainer = lightning.Trainer(max_epochs=2, accumulate_grad_batches=16)
-    trainer.fit(model=module, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader)
+    training_config = {'decoder_mode': decoder_mode,
+                       'wg': wg,
+                       'orig_batch_size': orig_batch_size,
+                       'batch_split': batch_split,
+                       'total_epochs': total_epochs,
+                       'lr': 0.05,
+                       'warmup_steps': 1000}
+    run_test(training_config, model_config, train_dataloader, valid_dataloader)
 
 
 def string_to_bool(s):
